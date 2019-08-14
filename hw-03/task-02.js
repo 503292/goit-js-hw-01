1b7a189ab6509509ff1d72000a693426114ac267
Kalysh <503292@ukr.net>
2019-08-15 01:32:37 +0300

    DONE _ task-02 - create function to count properties

--- hw-03/task-02.js
+++ hw-03/task-02.js
@@ -1,12 +1,26 @@
 "use strict";
 
 // Задание 2
-// Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
+// Напиши функцию countProps(obj), считающую кол-во свойств в объекте.
+// Функция возвращает число - количество свойств.
 
 // Вызовы функции для проверки работоспособности твоей реализации.
 
+const countProps = (obj) => {
+  let total = 0;
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    total++;
+  }
+
+  return total;
+};
+
 console.log(countProps({})); // 0
 
-console.log(countProps({ name: 'Mango', age: 2 })); // 2
+console.log(countProps({ name: "Mango", age: 2 })); // 2
 
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
+
+
+//DONE
