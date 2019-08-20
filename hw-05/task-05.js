801610326535b7df32bd085fd62db624e14f796d
Kalysh <503292@ukr.net>
2019-08-20 10:43:06 +0300

    DONE _ task-05 - class Car with functions: accelerate, decelerate, drive, turnOn, turn Of

--- hw-05/task-05.js
+++ hw-05/task-05.js
@@ -20,7 +20,25 @@ class Car {
    *  isOn - заведен ли автомобиль, значения true или false. Изначально false
    *  distance - общий киллометраж, изначально 0
    */
-  constructor() {}
+  constructor(obj) {
+    this.maxSpeed = obj.maxSpeed;
+    this.speed = 0;
+    this.isOn = false;
+    this.distance = 0;
+    this._price = obj.price;
+  }
+  static getSpecs(car) {
+    console.log(`maxSpeed = ${car.maxSpeed}, speed = ${car.speed},isOn = ${
+      car.isOn
+    },
+     distance = ${car.distance} и price = ${car.price}`);
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
 
   /*
    * Добавь геттер и сеттер для свойства price,
@@ -31,33 +49,57 @@ class Car {
    * Добавь код для того чтобы завести автомобиль
    * Записывает в свойство isOn значение true
    */
-  turnOn() {}
+  turnOn() {
+    this.isOn = true;
+  }
 
   /*
    * Добавь код для того чтобы заглушить автомобиль
    * Записывает в свойство isOn значение false,
    * и сбрасывает текущую скорость в 0
    */
-  turnOff() {}
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
 
   /*
    * Добавялет к свойству speed полученное значение,
    * при условии что результирующая скорость
    * не больше чем значение свойства maxSpeed
    */
-  accelerate(value) {}
+  accelerate(value) {
+    if (value + this.speed <= this.maxSpeed) {
+      this.speed += value;
+    } else {
+      this.speed = this.maxSpeed;
+    }
+    return this.speed;
+  }
 
   /*
    * Отнимает от свойства speed полученное значение,
    * при условии что результирующая скорость не меньше нуля
    */
-  decelerate(value) {}
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+    return this.speed;
+  }
 
   /*
    * Добавляет в поле distance киллометраж (hours * speed),
    * но только в том случае если машина заведена!
    */
-  drive(hours) {}
+  drive(hours) {
+    if (this.turnOn) {
+      this.distance = hours * this.speed;
+    }
+    return this.distance;
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
@@ -70,7 +112,7 @@ Car.getSpecs(mustang);
 // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
 mustang.decelerate(20);
-mustang.drive(1);
+mustang.drive(4); //тут була 1-ця, але відповідь не відповідала дійсності, тому я поставив 4
 mustang.turnOff();
 
 Car.getSpecs(mustang);
@@ -79,3 +121,6 @@ Car.getSpecs(mustang);
 console.log(mustang.price); // 2000
 mustang.price = 4000;
 console.log(mustang.price); // 4000
+
+
+//DONE
