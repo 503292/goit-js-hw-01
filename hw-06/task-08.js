b16b01b0e5defa31de99c09017e979c223bf948a
Kalysh <503292@ukr.net>
2019-08-21 15:48:11 +0300

    DONE _ task-08 - function to find getUserWithFriend

--- hw-06/task-08.js
+++ hw-06/task-08.js
@@ -1,11 +1,16 @@
 "use strict";
 
+import users from "./users.js";
+
 // Задание 8
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-const getUsersWithFriend = (users, friendName) => {
-  // твой код
-};
+const getUsersWithFriend = (users, friendName) =>
+  users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+//DONE
