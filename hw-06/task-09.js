eeb4e7b495b749709e79108a4bf4e38aa48acaf8
Kalysh <503292@ukr.net>
2019-08-21 16:01:48 +0300

    DONE _ task-09 - sort name by user.friends.length

--- hw-06/task-09.js
+++ hw-06/task-09.js
@@ -1,12 +1,14 @@
 "use strict";
 
+import users from "./users.js";
+
 // Задание 9
-// Массив имен (поле name) людей, 
+// Массив имен (поле name) людей,
 // отсортированных в зависимости от количества их друзей (поле friends)
 
-const getNamesSortedByFriendsCount = users => {
-  // твой код
-};
+const getNamesSortedByFriendsCount = users => users.sort((start, next) => start.friends.length > next.friends.length ? 1 : -1).map(user => user.name);
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+//DONE
