afe3b832e4aac8164b62629718572f9905366b5a
Kalysh <503292@ukr.net>
2019-08-21 15:27:03 +0300

    DONE _ task-06 - calculateTotalBalance with .reduce()

--- hw-06/task-07.js
+++ hw-06/task-07.js
@@ -1,10 +1,12 @@
 "use strict";
 
+import users from "./users.js";
+
 // Задание 7
 // Получить общую сумму баланса (поле balance) всех пользователей.
 
-const calculateTotalBalance = users => {
-  // твой код
-};
+const calculateTotalBalance = users => users.reduce((acc, user) => acc + user.balance, 0);
 
 console.log(calculateTotalBalance(users)); // 20916
+
+//DONE
