2430251c4c309cfbdc60b9571857bb8ea211c5c5
Kalysh <503292@ukr.net>
2019-09-04 21:16:58 +0300

    ADD _ changeTheme DARK/LIGHT

--- hw-10 dark-lite/src/index.js
+++ hw-10 dark-lite/src/index.js
@@ -1,39 +1,6 @@
-import menu from './menu.json';
-import menuTemplate from './templates/template.hbs';
+import './js/createMenu';
+import './js/changeTheme';
+
 import './styles.css';
 
-const refs = {
-  menuList: document.querySelector('#menu'),
-};
 
-// console.log(menu);
-// console.log(menuTemplate);
-
-// передаєм всі страви 
-const markup = menuTemplate(menu);
-// console.log(markup);
-
-refs.menuList.insertAdjacentHTML('beforeend', markup);
-
-// теми для сайта dark / light
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-console.log(localStorage);
-
-localStorage.setItem('theme', 'lite');
-
-
-// функція для перебору всього меню
-
-// buildMenu(menu);
-
-
-// function buildMenu(menu) {
-//   const markup = menu.map(dish => menuTemplate(dish)).join('');
-
-//   refs.menuList.insertAdjacentHTML('beforeend', markup);
-// //   console.log(markup);
-// }
