0a3e8b3fc723299bf892a87487e5b658bc76d722
Kalysh <503292@ukr.net>
2019-09-08 19:10:26 +0300

    DONE _ func which returns a promis that goes into resolved state in ms milliseconds.

--- hw-11/hw-11-02/01/script.js
+++ hw-11/hw-11-02/01/script.js
@@ -1,40 +1,17 @@
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548"
-];
-
-const refs = {
-  start: document.querySelector("[data-action='start']"),
-  stop: document.querySelector("[data-action='stop']"),
-  body: document.querySelector("body")
+const delay = ms => {
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`${ms}`);
+    }, ms);
+  });
+  return promise;
 };
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-let doInterval;
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
 
-const handelStart = e => {
-  doInterval = setInterval(() => {
-    // видає рандомне число з врахуванням довжини масиву
-    let rand = randomIntegerFromInterval(0, colors.length);
-    // console.log(rand);
-    // міняє колір заливки body
-    document.body.style.backgroundColor = colors[rand];
-  }, 1000);
-  refs.start.disabled = true;
-};
-
-const handelStop = e => {
-  clearInterval(doInterval);
-
-  refs.start.disabled = false;
-};
-
-refs.start.addEventListener("click", handelStart);
-refs.stop.addEventListener("click", handelStop);
+//DONE
