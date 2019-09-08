109a2141aced5ab407f935f56f61610ff312514a
Kalysh <503292@ukr.net>
2019-09-08 20:03:58 +0300

    DONE _ work with promise - revert users.active true/false by users.name

--- hw-11/hw-11-02/02/script.js
+++ hw-11/hw-11-02/02/script.js
@@ -1,40 +1,29 @@
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548"
+// Задание 2
+// Перепиши функцию toggleUserState() так,
+// чтобы она не использовала callback-функцию callback,
+// а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false }
 ];
 
-const refs = {
-  start: document.querySelector("[data-action='start']"),
-  stop: document.querySelector("[data-action='stop']"),
-  body: document.querySelector("body")
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const updatedUsers = allUsers.map(user => user.name === userName 
+        ? { ...user, active: !user.active } : user);
+      resolve(updatedUsers);
+    }, 1000);
+  });
 };
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const logger = updatedUsers => console.table(updatedUsers);
 
-let doInterval;
-
-const handelStart = e => {
-  doInterval = setInterval(() => {
-    // видає рандомне число з врахуванням довжини масиву
-    let rand = randomIntegerFromInterval(0, colors.length);
-    // console.log(rand);
-    // міняє колір заливки body
-    document.body.style.backgroundColor = colors[rand];
-  }, 1000);
-  refs.start.disabled = true;
-};
-
-const handelStop = e => {
-  clearInterval(doInterval);
-
-  refs.start.disabled = false;
-};
-
-refs.start.addEventListener("click", handelStart);
-refs.stop.addEventListener("click", handelStop);
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
